b402a62a3e393466cef4b9e0a260ed705b9a0015
JyothsnaKotyada <82897244+Jyothsna1899@users.noreply.github.com>
2023-06-02 15:51:36 +0530

    Update pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -13,7 +13,7 @@ done
 
 # A deploy step only if it's the master branch
 
-if [[ "$BUILDKITE_BRANCH" == "master" ]]; then
+if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
   echo "  - wait"
   echo "  - command: \"echo Deploy!\""
   echo "    label: \":rocket:\""
